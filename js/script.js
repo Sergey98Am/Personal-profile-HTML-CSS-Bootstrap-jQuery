4ecb65979479c6ef1acba9943d4ce5c8f1e0253f
Sergey98Am <serg98barca@gmail.com>
2021-11-12 11:47:40 +0400

    added sixth section

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ $(document).ready(function () {
         infinite: true,
         speed: 300,
         slidesToShow: 4,
-        prevArrow: '.prev-button',
-        nextArrow: '.next-button',
+        prevArrow: '.third-section .prev-button',
+        nextArrow: '.third-section .next-button',
         responsive: [
             {
                 breakpoint: 1024,
@@ -39,4 +39,37 @@ $(document).ready(function () {
         dots: true,
         arrows: false,
     })
+
+    $('.third-slider').slick({
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        prevArrow: '.sixth-section .prev-button',
+        nextArrow: '.sixth-section .next-button',
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
 });
