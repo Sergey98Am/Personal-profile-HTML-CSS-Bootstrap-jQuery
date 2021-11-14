e5103bfa2081f31d80f96a7f8f31106dc512b0d9
Sergey98Am <serg98barca@gmail.com>
2021-11-15 00:11:03 +0400

    added media on third section

--- js/script.js
+++ js/script.js
@@ -74,26 +74,23 @@ $(document).ready(function () {
         nextArrow: '.third-section .next-button',
         responsive: [
             {
-                breakpoint: 1024,
+                breakpoint: 1200,
                 settings: {
                     slidesToShow: 3,
-                    slidesToScroll: 3,
                     infinite: true,
                     dots: true
                 }
             },
             {
-                breakpoint: 600,
+                breakpoint: 991,
                 settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
+                    slidesToShow: 2
                 }
             },
             {
-                breakpoint: 480,
+                breakpoint: 768,
                 settings: {
                     slidesToShow: 1,
-                    slidesToScroll: 1
                 }
             }
         ]
